1aadbcf8f09aae3bb160ca78d0793e350bc6aaba
황인준 <os2864@naver.com>
2024-11-21 12:06:11 +0900

    :sparkles: feat: kakao map key 위치 등록

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,31 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-});
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  
+  return {
+    base: '/',
+    plugins: [
+      vue(),
+      vueDevTools(),
+      {
+        name: 'html-transform',
+        transformIndexHtml(html) {
+          return html.replace(
+            /%VITE_APP_KAKAO_MAP_API_KEY%/g,
+            env.VITE_APP_KAKAO_MAP_API_KEY
+          )
+        }
+      }
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
+  }
+})
